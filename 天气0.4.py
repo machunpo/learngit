d3b2a164f5bbc89bfe7a104b2cc132b11f1ac457
machunpo <machunpo@aliyun.com>
2019-02-14 10:56:34 +0800

    nothing

--- 天气0.4.py
+++ 天气0.4.py
@@ -70,15 +70,20 @@ while (True):
 
     yuyinbobao='今天是'+str(riqi[0])+'，'+hahhhh+'。'+'天气'+str(tianqi[0])+'。'+'气温'+str(low[0])+'到'+str(high[0])
     #print('今天是',riqi[0],'天气',tianqi[0],'气温',low[0]+'~'+high[0])
-    #print(yuyinbobao)
-    speak.Rate=-2
+    al=input()+"，，，，，，，，"
+    speak.Rate=-1
     #说话速度 -10到10
-    speak.Speak(yuyinbobao)
-    jia='出门，要记得带，雨伞啊。'
-    yi='出门，要记得涂，防晒霜啊。'
-    if ('雨' in str(tianqi[0])):
-        speak.Speak(jia)
+    if al=="，，，，，，，，":
+        speak.Speak(yuyinbobao)
+        jia='出门，要记得带，雨伞啊。'
+        yi='出门，要记得涂，防晒霜啊。'
+        jia1='出门，要记得带，雨伞，和保暖啊。'
+        if ('雨' in str(tianqi[0])):
+            speak.Speak(jia)
+        elif('雪' in str(tianqi[0])):
+            speak.Speak(jia1) 
+        else:
+            speak.Speak(yi)      
     else:
-        speak.Speak(yi)    
+        speak.Speak(al)
 
-    al=input()
