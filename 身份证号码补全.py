8a2b3939ac1663399ce78cfe2ec85bf8f81a4fb8
machunpo <machunpo@aliyun.com>
2019-02-28 16:35:29 +0800

    n

--- 身份证号码补全.py
+++ 身份证号码补全.py
@@ -1,1 +1,15 @@
-shenfenzhen_number=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+shenfenzhen_number=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+
+shenfenzhen_number_str=input()
+
+if len(shenfenzhen_number_str)!=18:
+
+    print("请输入正确的身份证号码。")
+
+else:
+    for i in shenfenzhen_number:
+
+        shenfenzhen_number[i]=shenfenzhen_number_str[i]
+
+        #print(shenfenzhen_number[i])
+        #[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
