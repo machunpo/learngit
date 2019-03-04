46104e5c2441d6c13f719a0b1eec1130bc0c9423
machunpo <machunpo@aliyun.com>
2019-03-04 11:28:15 +0800

    n

--- 身份证号码补全.py
+++ 身份证号码补全.py
@@ -18,47 +18,22 @@
 '''
 errorInfo = ""# 记录错误信息
 
-onetwowei = {"11":"北京","12":"天津","13":"河北","14":"山西","15":"内蒙古","21":"辽宁","22":"吉林","23":"黑龙江"}
-      '''   
-       
-     
-        hashtable.put("31", "上海");
-        hashtable.put("32", "江苏");
-        hashtable.put("33", "浙江");
-        hashtable.put("34", "安徽");
-        hashtable.put("35", "福建");
-        hashtable.put("36", "江西");
-        hashtable.put("37", "山东");
-        hashtable.put("41", "河南");
-        hashtable.put("42", "湖北");
-        hashtable.put("43", "湖南");
-        hashtable.put("44", "广东");
-        hashtable.put("45", "广西");
-        hashtable.put("46", "海南");
-        hashtable.put("50", "重庆");
-        hashtable.put("51", "四川");
-        hashtable.put("52", "贵州");
-        hashtable.put("53", "云南");
-        hashtable.put("54", "西藏");
-        hashtable.put("61", "陕西");
-        hashtable.put("62", "甘肃");
-        hashtable.put("63", "青海");
-        hashtable.put("64", "宁夏");
-        hashtable.put("65", "新疆");
-        hashtable.put("71", "台湾");
-        hashtable.put("81", "香港");
-        hashtable.put("82", "澳门");
-        hashtable.put("91", "国外");
-'''
+
 
 shenfenzhen_number=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 
 shenfenzhen_number_str=input()
 
 if len(shenfenzhen_number_str)!=18:
+    errorInfo = "身份证号码长度应该为18位。"
+    
+if (shenfenzhen_number_str[0:16].isnumeric() == False):
+    errorInfo = "身份证18位号码除最后一位外，都应为数字。"
 
-    print("请输入正确的身份证号码。")
+print(errorInfo)
 
+
+'''
 else:
     for i in shenfenzhen_number:
 
@@ -66,3 +41,6 @@ else:
 
         #print(shenfenzhen_number[i])
         #[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+
+
+'''
