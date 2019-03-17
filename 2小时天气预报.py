5737474d9ea70d583763614fb58307a3f984a3f9
machunpo <machunpo@aliyun.com>
2019-03-17 16:21:11 +0800

    使用彩云天气的网页进行爬取

--- 2小时天气预报.py
+++ 2小时天气预报.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import urllib.request
+back_up_url='http://caiyunapp.com/map/#120.4768,32.5131'        #这是彩云天气的短时间预报
 
 #使用的是常州站的数据
-url_pic='http://products.weather.com.cn/product/radar/index/procode/JC_RADAR_AZ9519_JB.shtml'
+url_of_pic='http://products.weather.com.cn/product/radar/index/procode/JC_RADAR_AZ9519_JB.shtml'
 
 
 
@@ -20,9 +21,9 @@ def extract(string_all,string_begin,string_end):  #提取数据的函数
     return string_arry
 
 
-response = urllib.request.urlopen(url_pic)
-content = response.read().decode('gbk', 'ignore')#打开要抓取的网页
+response = urllib.request.urlopen(back_up_url)
+content = response.read()#.decode('gbk', 'ignore')#打开要抓取的网页
 
-print(content)
-
-#extract(content,'begin','end')
+print(content.decode('utf-8'))
+print(back_up_url)
+#extract(content,'begin','end')“
