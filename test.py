91456840cd6d54f370533160077829d069e9aa03
machunpo <machunpo@aliyun.com>
2019-03-27 17:13:58 +0800

    little

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import my_def_lib
+import json
+import urllib.request
+import random
 
-num=10
-print('num = ',my_def_lib.checkTime(num))
-print('num-1 = ',my_def_lib.checkTime(num-1)) 
+random_number=0
+url="https://api.caiyunapp.com/v2/place?token=token&count=1&lang=zh_CN&query=%E6%B5%B7%E5%AE%89%E5%8E%BF&random=2"
+response = urllib.request.urlopen(url)
+content = response.read().decode('gbk', 'ignore')
+new_dict = json.loads(content)
+print(new_dict)
+
