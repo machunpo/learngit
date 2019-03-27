61c04e0cc8b47620e8c8a1f19486021682fd3586
machunpo <machunpo@aliyun.com>
2019-03-27 16:53:35 +0800

    小修改

--- my_def_lib.py
+++ my_def_lib.py
@@ -16,7 +16,7 @@ import json
 import urllib.request
 
 
-#函数功能：通过彩云app获得本机IP地址（互联网）等
+#函数功能：通过彩云app获得本机IP地址（互联网）经纬度坐标，城市，返回状态等
 #输入参数：无
 #输出参数：无
 def getip():
@@ -25,10 +25,12 @@ def getip():
     content = response.read().decode('gbk', 'ignore')
     new_dict = json.loads(content)
     #print(new_dict)
+    
     status=new_dict['status']
     center=new_dict['center']
     city=new_dict['city']
     ip=new_dict['ip']
+
     print('status:',status)
     print('ip:',ip)
     print('center:',center)
