6f65eb1b8f8a548eb7eb022fa472de819afb07f8
machunpo <machunpo@aliyun.com>
2019-05-08 14:14:21 +0800

    add dizhen

--- dizhen.py
+++ dizhen.py
@@ -20,14 +20,31 @@ def extract(string_all,string_begin,string_end):
 url = 'http://news.ceic.ac.cn'
 
 
-
-
 response = urllib.request.urlopen(url)
 content = response.read().decode('utf-8', 'ignore')
-#print(content)
+
 
 提取的信息=extract(content,'<td align="center" style="padding-left: 20p','></td>')
 
-print(提取的信息[0])
-print(提取的信息[1])
-print(提取的信息[2])
+#print(提取的信息[0])
+#print(提取的信息[1])
+#print(提取的信息[2])
+'''
+x">5.2</td>
+                        <td align="center" style="width: 155px;">2019-04-08 12:1
+2:40</td>
+                        <td align="center">26.30</td>
+                        <td align="center">129.50</td>
+                        <td align="center">20</td>
+                        <td align="left"><a href="http://news.ceic.ac.cn/CC20190
+408121240.html">琉球群岛</a
+
+'''
+msg_cls=[] #存储归类好的信息
+
+for i in 提取的信息:
+    #print(i)
+    print(type(i))
+    print('\n')
+    print(extract(i,'">','</'))
+
