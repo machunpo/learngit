4c6bc40f7850cb7d924e9aa797439c1407625824
machunpo <machunpo@aliyun.com>
2019-05-08 16:13:21 +0800

    基本可以上线运行

--- dizhen.py
+++ dizhen.py
@@ -1,5 +1,9 @@
 import urllib.request
+import operator
+import time
+import win32com.client as win #pip install pypiwin32
 
+speak = win.Dispatch("SAPI.SpVoice")  #增加语音播报的模块
 
 def extract(string_all,string_begin,string_end):
     m=0;n=0;string_arry=[]
@@ -15,36 +19,69 @@ def extract(string_all,string_begin,string_end):
     return string_arry[:-1] #有个bug，最后一个数组元素内存溢出
 
 
+def 获取最新地震信息():
+
+    url = 'http://news.ceic.ac.cn'
+
+    response = urllib.request.urlopen(url)
+    content = response.read().decode('utf-8', 'ignore')
 
 
-url = 'http://news.ceic.ac.cn'
+    提取的信息=extract(content,'<td align="center" style="padding-left: 20p','></td>')
+
+    #['3.6', '2019-05-08 04:39:51', '37.37', '95.25', '9', '<a href="http://news.ceic.ac.cn/CD20190508043952.html">青海海西州直辖区']
+
+    #震级(M) , 发震时刻(UTC+8) , 纬度(°) , 经度(°) , 深度(千米) , 参考位置
+
+    msg_cls=[] #存储归类好的信息
+
+    for i in 提取的信息:
+    
+        msg_cls.append(extract(i,'">','</'))
+    '''
+    for i in msg_cls:
+        for j in i:
+            print(j)
+    '''
+    最新信息=msg_cls[0]
+    return(最新信息)
+
+if __name__ == '__main__':
+
+    print("地震实时播报系统")
+    地震信息=[]
+    #地震信息=获取最新地震信息()
+    i=0#计数器
+    delay=60#延时的时间
+    
+    while 1 :
+
+        #print(获取最新地震信息())
+        #地点=地震信息[5]
+        #地点=地点[地点.find('>')+1:]
+        #print(地点)
+        
+        i=i+delay
+        print('系统运行{}秒'.format(i))
+
+        if(operator.eq(地震信息,获取最新地震信息())):
+            print('未接收到地震信息')
+        else: 
+            地震信息=获取最新地震信息()
+
+            地点=地震信息[5]
+
+            地点=地点[地点.find('>')+1:]
+
+            print(地震信息)
+            播报信息='最新播报:'+地震信息[1]+'秒在'+地点+'发生'+地震信息[0]+'级地震'
+            speak.Speak(播报信息)
+            continue
+
+        time.sleep(delay)
 
 
-response = urllib.request.urlopen(url)
-content = response.read().decode('utf-8', 'ignore')
 
 
-提取的信息=extract(content,'<td align="center" style="padding-left: 20p','></td>')
 
-#print(提取的信息[0])
-#print(提取的信息[1])
-#print(提取的信息[2])
-'''
-x">5.2</td>
-                        <td align="center" style="width: 155px;">2019-04-08 12:1
-2:40</td>
-                        <td align="center">26.30</td>
-                        <td align="center">129.50</td>
-                        <td align="center">20</td>
-                        <td align="left"><a href="http://news.ceic.ac.cn/CC20190
-408121240.html">琉球群岛</a
-
-'''
-msg_cls=[] #存储归类好的信息
-
-for i in 提取的信息:
-    #print(i)
-    print(type(i))
-    print('\n')
-    print(extract(i,'">','</'))
 
