2f9fdb9637ef256a294ed872d8f59dc0b65fc880
machunpo <machunpo@aliyun.com>
2019-07-29 15:20:47 +0800

    2 hour

--- 天气0.4.py
+++ 天气0.4.py
@@ -5,6 +5,8 @@ import datetime
 import win32com.client as win #pip install pypiwin32
 import json
 from my_def_lib import extract
+import time
+
 
 speak = win.Dispatch("SAPI.SpVoice")  #增加语音播报的模块
 weekchn=['星期一','星期二','星期三','星期四','星期五','星期六','星期日','星期一','星期二','星期三','星期四','星期五','星期六','星期日','星期一','星期二','星期三','星期四','星期五','星期六','星期天']
@@ -13,10 +15,41 @@ al="，，，，，，，，"
 speak.Rate=-1      #说话速度 -10到10
 WANGZHIQIANZUI="http://www.mxnzp.com/api/holiday/single/"
 
+def two_hour():
+
+    back_up_url='https://api.caiyunapp.com/v2/96Ly7wgKGq6FhllM/120.4769,32.5131/weather.jsonp?hourlysteps=120'        #这是彩云天气的短时间预报
+    jizhunshuju='这是用来比较是否更新的基础数据'
+    i=1
+    #使用的是常州站的数据
+    #url_of_pic='http://products.weather.com.cn/product/radar/index/procode/JC_RADAR_AZ9519_JB.shtml'
+
+    while(1):
+
+        response = urllib.request.urlopen(back_up_url)
+        content = response.read().decode('unicode_escape')#打开要抓取的网页
+
+        new_dict = json.loads(content)
+        tianqiyubao=new_dict["result"]["forecast_keypoint"]
+
+        if tianqiyubao != jizhunshuju:
+            print(tianqiyubao)
+            jizhunshuju=tianqiyubao
+        else:
+            pass
+        
+        print(i)
+        #print(new_dict["result"]["minutely"]["description"])
+        #import sys
+        #print('目前系统的编码为：',sys.getdefaultencoding())
+        #print('\\u672a\\u676524\\u5c0f\\u65f6\\u591a\\u4e91'.encode('latin-1').decode('unicode_escape'))
+        i=i+1
+        time.sleep(10)
 
 
 
 while (True):
+
+
    
     d=datetime.date.weekday(datetime.date.today())         #添加星期几的功能
 
@@ -48,7 +81,7 @@ while (True):
 
     
 
-#本段代码用来增加农历日期的功能
+    #本段代码用来增加农历日期的功能
 
     print()
 
