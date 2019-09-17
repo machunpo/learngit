563571b5e5cdfd17eb02bb7561bae3cdfb57a15d
machunpo <machunpo@aliyun.com>
2019-09-17 21:18:47 +0800

    #下一步是提取文件的拍摄日期是什么时候   也就是exifread的用法

--- pic.py
+++ pic.py
@@ -1,25 +1,32 @@
 import os
-#import exifread
+import exifread
 
 Source_folder="c:\\Source"
 Target_folder="c:\\Target"
 
-
+#函数名称：get_file_extension  提取文件名的扩展名
+#入口参数：filename  字符串 文件名
+#返回参数：filename[-i+1:] 文件的扩展名
 def get_file_extension(filename):
-    for i in reversed(filename):
-        print(i)
+    i=0
+    for character in reversed(filename):
+        i=i+1
+        if  character == ".":
+            break
+    return(filename[-i+1:])
+    #print(i,filename[-i+1:])
 
 
 
 
 for root, dirs, files  in os.walk(Source_folder):
-    for f in files:
-            path=root+"\\"+f
-            print(path)
+    for my_file in files:
+        if (get_file_extension(my_file)=="jpg"):#如果文件的后缀名为jpg
+
+            path=root+"\\"+my_file
+            print(path)                             #打印出完整的文件路径
     #print(root, dirs, files )
 
-str1="abcdefghijklmnopqrstuvwxyz.txt"
-get_file_extension(str1)
-print(str1)
 
-#下一步是辨别文件的后缀名是什么
+
+#下一步是提取文件的拍摄日期是什么时候   也就是exifread的用法
