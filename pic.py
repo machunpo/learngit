9addfa7721a12c81d431cce3967131709ea77f23
machunpo <machunpo@aliyun.com>
2019-09-19 21:06:57 +0800

    #下一步是剪切文件 和新建文件夹的操作

--- pic.py
+++ pic.py
@@ -18,24 +18,26 @@ def get_file_extension(filename):
 
 #函数名称：get_yearmoon_of_pic  提取图片的拍摄日期中的年和月
 #入口参数：picpath              图片的地址路径
-#返回参数：
+#返回参数：形如[['2010', '11', '11'], ['00', '13', '48']]的二维数组
 def get_yearmoon_of_pic(picpath):
     f = open(picpath, 'rb')
     tags = exifread.process_file(f)
+    data_arry=tags['Image DateTime'].values.split(" ")
     f.close()
-    return(str(tags['Image DateTime']))
+    return([x.split(":") for x in data_arry])
 
 
 for root, dirs, files  in os.walk(Source_folder):
     for my_file in files:
-        if (get_file_extension(my_file)=="jpg"):#如果文件的后缀名为jpg
+        if (get_file_extension(my_file)=="jpg"):  #如果文件的后缀名为jpg   这里应该用try
 
             path=root+"\\"+my_file
-            print(path)                             #打印出完整的文件路径
 
-            print(get_yearmoon_of_pic(path))
+            print(path)                             #打印出完整的文件路径
+            year_moon=get_yearmoon_of_pic(path)
+            print(year_moon[0][0],year_moon[0][1])        #打印出照片拍摄的年和月（处理好的）
     #print(root, dirs, files )
 
-input()
+#input()
 
-#下一步是分割字符串 把年月的信息提取出来
+#下一步是剪切文件 和新建文件夹的操作
--- test/exif.py
+++ test/exif.py
@@ -5,10 +5,15 @@ f = open(r'C:\Source\1\192409110.jpg', 'rb')
 # Return Exif tags
 tags = exifread.process_file(f)
 
-print(tags['Image DateTime'])
+print(type(tags['Image DateTime'].values))
+print('date:',tags['Image DateTime'].values)
 
-print(str(tags['Image DateTime'])[:4])
+print('date_part_of_str:',str(tags['Image DateTime'])[:4])
 
-print(dir(exifread.classes))
+print('theclass',dir(tags['Image DateTime']))
 
-print(exifread.classes)
+
+
+data_arry=tags['Image DateTime'].values.split(" ")
+
+print([x.split(":") for x in data_arry])
