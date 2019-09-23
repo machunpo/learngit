19efbf139b7214d2fe14a872bbbbfbe6cc0bcf28
machunpo <machunpo@aliyun.com>
2019-09-23 17:52:20 +0800

    #下一步是对文件的拷贝

--- pic.py
+++ pic.py
@@ -3,19 +3,26 @@ import exifread
 
 Source_folder="c:\\Source"
 Target_folder="c:\\Target"
-pic_type=['jpg','gif']
+pic_type=['jpg','gif','bmp','png','jpeg']
 
 #函数名称：get_file_extension  提取文件名的扩展名
 #入口参数：filename  字符串 文件名
 #返回参数：filename[-i+1:] 文件的扩展名
 def get_file_extension(filename):
     i=0
+    j=False
     for character in reversed(filename):
         i=i+1
         if  character == ".":
+            j=True
             break
-    return(filename[-i+1:])
-    #print(i,filename[-i+1:])
+    if j:
+        return(filename[-i+1:])
+    else:
+        return('这个字符串肯定不是哪个文件的后缀名')
+
+    
+
 
 #函数名称：get_yearmoon_of_pic  提取图片的拍摄日期中的年和月
 #入口参数：picpath              图片的地址路径
@@ -38,11 +45,11 @@ for root, dirs, files  in os.walk(Source_folder):
     for my_file in files:
 
         try:
-            pass
+            houzhuiming_file=get_file_extension(my_file)
         except :
-            pass
+            print('没有获取到这个文件的扩展名')
 
-        if (get_file_extension(my_file) in pic_type):  #如果文件的后缀名 in pic_type     这里要考虑如果提取不到文件的后缀名
+        if (houzhuiming_file in pic_type):  #如果文件的后缀名 in pic_type     这里要考虑如果提取不到文件的后缀名ok
 
             Source_file_path=root+"\\"+my_file
 
@@ -52,21 +59,26 @@ for root, dirs, files  in os.walk(Source_folder):
             #print(year,moon)
             
             #name_of_targer_dir=year+'年'+moon+'月'
-            Target_file_path=(Target_folder+'\\'+year+'年'+moon+'月'+'\\')
+            Target_dir_path=(Target_folder+'\\'+year+'年'+moon+'月'+'\\')
+            Target_file_path=Target_dir_path+my_file
             
-            is_target_dir=os.path.exists(Target_file_path)#目标文件夹是否存在
-            is_target_file='备用变量'
+            is_target_dir=os.path.exists(Target_dir_path)#目标文件夹是否存在
+            is_target_file=os.path.exists(Target_file_path)#目标文件是否存在
 
             if (is_target_dir):     #目标文件夹如果存在
-                #print(Target_file_path,'存在')
+                
+                if is_target_file:
+                    pass#改名再拷贝
+                else:
+                    pass#直接拷贝
                 print('目标文件夹存在')
             else:                 #目标文件夹如果不存在  
-                os.mkdir(Target_file_path) #则创建目录
-                print('创建'  '{}'  '目录'.format(Target_file_path))
+                os.mkdir(Target_dir_path) #则创建目录
+                print('创建'  '{}'  '目录'.format(Target_dir_path))
                 #print(is_target_dir)
 
    
 
 input()
 
-#下一步是考虑各种情况对文件的拷贝
+#下一步是对文件的拷贝
