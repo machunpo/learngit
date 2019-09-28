fd98a452abe5d89bd6806836106ac4a384ab1242
machunpo <machunpo@aliyun.com>
2019-09-28 20:26:55 +0800

    #熟悉一下pyautogui模块的各个类和函数   安装pyautogui模块 #pip 更换源为阿里或者清华

---
+++ pygame.py
@@ -0,0 +1,20 @@
+import pyautogui as pyauto
+
+
+print(pyauto.__doc__)
+print(pyauto.size())
+
+o=(600,600)
+pyauto.moveTo(o)
+
+
+
+
+
+
+
+
+
+
+#熟悉一下pyautogui模块的各个类和函数   安装pyautogui模块
+#pip 更换源为阿里或者清华
