59b75b42fb2dff1f7a55a4a287f0f7be7673c07f
machunpo <machunpo@aliyun.com>
2019-09-29 17:51:57 +0800

    #对流程进行规划

--- pygame.py
+++ pygame.py
@@ -1,20 +1,24 @@
 import pyautogui as pyauto
+import time
 
-
-print(pyauto.__doc__)
+time.sleep(5)  #   延时5秒 用于开始的操作
 print(pyauto.size())
 
-o=(600,600)
-pyauto.moveTo(o)
+path_pic=r'C:\Users\zyf\Desktop\新建文件夹\申请试用.png'
 
 
+o=pyauto.locateOnScreen(path_pic)
+if o:
+    print(o)
 
+    x,y=pyauto.center(o)
 
+    print(x,y)
+    pyauto.moveTo(x,y)
+    time.sleep(3)
+    pyauto.click()
 
+time.sleep(3)
 
-
-
-
-
-#熟悉一下pyautogui模块的各个类和函数  
+#对流程进行规划
 
