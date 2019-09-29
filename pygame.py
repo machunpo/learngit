d4cf7c1607ff37a47a6de0a4b7b4ae4fbcc42311
machunpo <machunpo@aliyun.com>
2019-09-29 21:27:08 +0800

    #pyauto.locateOnScreen  的返回值是多少  从第二十3行开始写

--- pygame.py
+++ pygame.py
@@ -1,18 +1,29 @@
 import pyautogui as pyauto
 import time
+import os
 
-time.sleep(5)  #   延时5秒 用于开始的操作
-print(pyauto.size())
+time.sleep(5)                       # 延时5秒 用于开始的操作
 
-path_pic=r'C:\Users\zyf\Desktop\新建文件夹\申请试用.png'
+print(pyauto.size())                # 当前屏幕的分辨率
 
+current_directory=os.getcwd()       #获取当前目录
+print(current_directory)
 
-o=pyauto.locateOnScreen(path_pic)
-if o:
-    print(o)
+images_directory='\\images\\'       #定义图片目录
+pic_A=r'申请试用.png'                #定义识别的图片
+pic_B=r''
+pic_C=r''
+pic_D=r''
+pic_E=r''
+pic_F=r''
 
-    x,y=pyauto.center(o)
+path_pic_A=current_directory+images_directory+pic_A
+print(path_pic_A)                                                  #当前目录\images\申请试用.png
 
+A=pyauto.locateOnScreen(path_pic_A)
+if A:
+    print(A)
+    x,y=pyauto.center(A)
     print(x,y)
     pyauto.moveTo(x,y)
     time.sleep(3)
@@ -20,5 +31,5 @@ if o:
 
 time.sleep(3)
 
-#对流程进行规划
+#pyauto.locateOnScreen  的返回值是多少  从第二十3行开始写
 
