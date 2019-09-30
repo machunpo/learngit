1149f94eef5059adf3521b764d6e70995adf09dc
machunpo <machunpo@aliyun.com>
2019-09-30 15:42:14 +0800

    #此处要把鼠标移走

--- pygame.py
+++ pygame.py
@@ -8,28 +8,55 @@ print(pyauto.size())                # 当前屏幕的分辨率
 
 current_directory=os.getcwd()       #获取当前目录
 print(current_directory)
-
 images_directory='\\images\\'       #定义图片目录
+
 pic_A=r'申请试用.png'                #定义识别的图片
-pic_B=r''
-pic_C=r''
-pic_D=r''
+pic_B=r'关注并申请.png'
+pic_C=r'申请成功.png'
+pic_D=r'当前用户未关注店铺.png'
 pic_E=r''
 pic_F=r''
 
 path_pic_A=current_directory+images_directory+pic_A
-print(path_pic_A)                                                  #当前目录\images\申请试用.png
+path_pic_B=current_directory+images_directory+pic_B
+path_pic_C=current_directory+images_directory+pic_C
+path_pic_D=current_directory+images_directory+pic_D
 
-A=pyauto.locateOnScreen(path_pic_A)
-if A:
-    print('A type is',type(A))
-    x,y=pyauto.center(A)
-    print(x,y)
-    pyauto.moveTo(x,y)
+for i in range(10):
+    #此处要把鼠标移走                                           
+    A=pyauto.locateOnScreen(path_pic_A)#申请试用
+    if A:
+        pyauto.moveTo(pyauto.center(A))
+        time.sleep(3)
+        pyauto.click()
+
     time.sleep(3)
-    pyauto.click()
+      
+    B=pyauto.locateOnScreen(path_pic_B)#关注并申请
+    if B:
+        pyauto.moveTo(pyauto.center(B))
+        time.sleep(3)
+        pyauto.click()
 
-time.sleep(3)
+    time.sleep(3)
 
-#pyauto.locateOnScreen  的返回值是多少  从第二十3行开始写
+
+    C=pyauto.locateOnScreen(path_pic_C)#申请成功
+    if C:
+       
+        time.sleep(3)
+        pyauto.hotkey('ctrl','w')
+
+    time.sleep(3)
+
+    D=pyauto.locateOnScreen(path_pic_D)#当前用户未关注店铺
+    if D:
+       
+        time.sleep(3)
+        pyauto.hotkey('f5')
+
+    time.sleep(3)
+
+
+
 
