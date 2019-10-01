d3e93fa65de600df5c201b4586c2d3eba819924b
machunpo <machunpo@aliyun.com>
2019-10-01 12:37:20 +0800

    '你的申请已成功提交'

--- pygame.py
+++ pygame.py
@@ -14,40 +14,40 @@ pic_A=r'申请试用.png'                #定义识别的图片
 pic_B=r'关注并申请.png'
 pic_C=r'申请成功.png'
 pic_D=r'当前用户未关注店铺.png'
-pic_E=r''
-pic_F=r''
+pic_E=r'操作不要太快吆.png'
+pic_F=r'你的申请已成功提交'
 
 path_pic_A=current_directory+images_directory+pic_A
 path_pic_B=current_directory+images_directory+pic_B
 path_pic_C=current_directory+images_directory+pic_C
 path_pic_D=current_directory+images_directory+pic_D
 
-for i in range(10):
-    #此处要把鼠标移走                                           
+for i in range(100):
+
     A=pyauto.locateOnScreen(path_pic_A)#申请试用
     if A:
         pyauto.moveTo(pyauto.center(A))
-        time.sleep(3)
+        time.sleep(2)
         pyauto.click()
 
-    time.sleep(3)
+    time.sleep(2)
       
     B=pyauto.locateOnScreen(path_pic_B)#关注并申请
     if B:
         pyauto.moveTo(pyauto.center(B))
-        time.sleep(3)
+        time.sleep(2)
         pyauto.click()
 
-    time.sleep(3)
+    time.sleep(2)
 
 
     C=pyauto.locateOnScreen(path_pic_C)#申请成功
     if C:
        
-        time.sleep(3)
+        time.sleep(2)
         pyauto.hotkey('ctrl','w')
 
-    time.sleep(3)
+    time.sleep(2)
 
     D=pyauto.locateOnScreen(path_pic_D)#当前用户未关注店铺
     if D:
@@ -55,8 +55,10 @@ for i in range(10):
         time.sleep(3)
         pyauto.hotkey('f5')
 
-    time.sleep(3)
+    time.sleep(2)
 
-
+    pyauto.moveTo(10,10) #回到待命位置
+    time.sleep(2)
+    print('回到初始点')
 
 
