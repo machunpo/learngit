c7740ab8e3455d181aee2e1ef6fb7ca86ad236c4
machunpo <machunpo@aliyun.com>
2019-10-12 14:22:35 +0800

    pyauto.PAUSE=1.5

--- pygame.py
+++ pygame.py
@@ -3,6 +3,7 @@ import time
 import os
 
 time.sleep(3)                       # 延时3秒 用于开始的操作
+pyauto.PAUSE=1.5
 
 print(pyauto.size())                # 当前屏幕的分辨率
 
@@ -32,41 +33,42 @@ for i in range(100):
     
     if A:
         pyauto.moveTo(pyauto.center(A))
-        time.sleep(1)
+        
         pyauto.click()
-        time.sleep(1)
+        
     elif pyauto.locateOnScreen(path_pic_B):#关注并申请
         pyauto.moveTo(pyauto.center(pyauto.locateOnScreen(path_pic_B)))
-        time.sleep(1)
+        
         pyauto.click()
-        time.sleep(1)
+        
     else:
         if pyauto.locateOnScreen(path_pic_C): #申请成功
-            time.sleep(2)
+            
             pyauto.hotkey('ctrl','w')
-            time.sleep(2)
+            
         else:
              if pyauto.locateOnScreen(path_pic_D):#当前用户未关注店铺
-                time.sleep(2)
+                
                 pyauto.hotkey('f5')
-                time.sleep(2)
+                
              else:              
                 if pyauto.locateOnScreen(path_pic_E):#操作不要太快吆
-                    time.sleep(2)
+                    
                     pyauto.hotkey('f5')
-                    time.sleep(2)
+                    
                 elif pyauto.locateOnScreen(path_pic_F):#你的申请已成功提交
-                    time.sleep(2)
+                    
                     pyauto.hotkey('ctrl','w')
-                    time.sleep(2)
+                    
 
        
 
 
-    time.sleep(2)
+  
     pyauto.moveTo(10,10) #回到待命位置
-    time.sleep(2)
+  
     
 print('game over.')
+
 #下一步是要重构 1.改成列表 2.写成函数，根据输入的参数来进行操作
 
