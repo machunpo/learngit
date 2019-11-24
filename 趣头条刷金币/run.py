e76720d62d1f5ec56ba5936639f10f9e43a1baf5
machunpo <machunpo@aliyun.com>
2019-11-24 17:08:05 +0800

    增加刷视频功能

--- 趣头条刷金币/run.py
+++ 趣头条刷金币/run.py
@@ -6,7 +6,8 @@ import win32com.client as win
 speak = win.Dispatch("SAPI.SpVoice")  
 speak.Rate=-1 
 #增加语音播报的模块
-loop_time=35
+loop_time_news=1
+loop_time_video=35
 
 def put_page_up():
     os.system('adb shell input swipe 320 410 320 1000 500')  #//从 320 410 经历0.5秒滑动到 320 1000  手指向下滑
@@ -46,7 +47,7 @@ if __name__ == '__main__':
     os.system('adb devices')
     os.system('adb version')
 
-    for i in range(loop_time):
+    for i in range(loop_time_news):
     
 
         if cheak_adb_link('adb shell input swipe 320 410 320 1000 500'):
@@ -87,17 +88,42 @@ if __name__ == '__main__':
             put_page_up()
             time.sleep(3)
 
-        speak_and_print('共{}次，{}结束第{}次'.format(loop_time,chengong_or_shibai,i+1))
+        speak_and_print('共{}次，{}结束第{}次'.format(loop_time_news,chengong_or_shibai,i+1))
         time.sleep(10)
 
         print(iskongbai[0]==255 and isdingwei[0]==243)
-        #print(iskongbai[0])#255.255.255 255
-        #print(isdingwei[0])#(243, 247, 246, 255)
-        #下面写逻辑
-        #240   460
-        #480   460
  
-        #500   100
+    #下面是刷视频
+    os.system('adb shell input tap 216 1220') 
+
+    for i in range(loop_time_video):
+    
+
+        if cheak_adb_link('adb shell input swipe 320 410 320 1000 500'):
+            os.system('adb devices')
+            os.system('adb version')
+            os.system('adb kill-server')
+            #adb kill-server,adb start-server,
+            cmd='手机链接出问题了，重新链接一下。'
+            speak_and_print(cmd)
+            #os.system('adb shell')
+            os.system('adb shell input swipe 320 410 320 1000 500')
+
+        time.sleep(10)#等待顶部的更新条消失
+        pull_screenshot()
+        isbaishesanjiaoxin=get_pixel_colour(r'C:\Users\machunpo\Desktop\images\funtoutiao.png',355,305)
+
+        if isbaishesanjiaoxin[0]==255 :
+            os.system('adb shell input tap 355 305') 
+            time.sleep(100)
+        else:
+            put_page_up()
+            time.sleep(2)
+            put_page_up()
+            time.sleep(8)
+
+        speak_and_print('共{}次，结束第{}次'.format(loop_time_video,i+1))
+
     speak_and_print('本次卫星发射圆满成功。')
 
     #下一次把参数传进函数
