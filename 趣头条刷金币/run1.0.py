6aa7a19de924a082466d718debf218d7e2665511
machunpo <machunpo@aliyun.com>
2020-01-04 18:22:02 +0800

    litteler changer

--- 趣头条刷金币/run1.0.py
+++ 趣头条刷金币/run1.0.py
@@ -9,6 +9,8 @@ speak.Rate=-1
 loop_time_news=15
 loop_time_video=5
 
+loop_time_news=input()
+
 def put_page_up():
     os.system('adb shell input swipe 320 410 320 1000 500')  #//从 320 410 经历0.5秒滑动到 320 1000  手指向下滑
 
