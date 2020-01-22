d9915e7e6db6e763e28f80b4b6db17af08dd3233
machunpo <machunpo@aliyun.com>
2020-01-22 09:38:53 +0800

    jihe

--- whfybb.py
+++ whfybb.py
@@ -4,7 +4,7 @@ import time
 import win32com.client as win  # pip install pypiwin32
 
 speak = win.Dispatch("SAPI.SpVoice")  #增加语音播报的模块
-speak.Rate=-3      #说话速度 -10到10
+speak.Rate=-2     #说话速度 -10到10
 
 url='https://3g.dxy.cn/newh5/view/pneumonia'#丁香园关于疫情的实时播报
 
@@ -15,13 +15,18 @@ while (1):
     uft_str = uft_str.encode("iso-8859-1").decode('utf8')#.encode('utf8')
 
     list_1=my_def_lib.extract(uft_str,'<p class="topicTitle___2ovVO">','</p>')
-
     news_feiyang=my_def_lib.quchu_heml(list_1[0])
     print(news_feiyang)
+    
+    list_2=my_def_lib.extract(uft_str,'<p class="topicContent___1KVfy">','</p>')
+    news_feiyang1=my_def_lib.quchu_heml(list_2[0])
+    print(news_feiyang1)
+
     #my_def_lib.speak_and_print(news_feiyang)
     if (duibi != news_feiyang) :
 
         speak.Speak(news_feiyang)
+        speak.Speak(news_feiyang1)
         duibi=news_feiyang
     else:
         time.sleep(300)
@@ -30,8 +35,10 @@ while (1):
 
 
 #<p class="topicTitle___2ovVO"><i>最新</i>湖北启动突发公共卫生事件二级应急响应</p>
+'''
+22 日凌晨，湖北发布加强新型肺炎防控通告：根据《中华人民共和国传染病防治法》等有关法律法规的规定，省人民政府决定启动突发公共卫生事件 II 级应急响应。通告包括严格实行属地管理制度、严格实施隔离措施等7部分。</p>
 
- 
+ '''
 # 查看响应内容，response.content返回的字节流数据
 #print (respones.content)
  
