2a98664d309801307161c854434758baf3dba3a0
machunpo <machunpo@aliyun.com>
2020-03-15 12:55:26 +0800

    #这个库不支持中文目录

---
+++ 京东试用/jd_test.py
@@ -0,0 +1,28 @@
+import pyautogui as pyauto #这个库不支持中文目录
+import time
+import os
+import random
+
+loop_time=80                        #运行的次数
+pyauto.PAUSE=0.5                    #每次pyauto的延时
+
+
+def setup():
+    print(pyauto.size())                # 当前屏幕的分辨率
+    print('当前目录：',os.getcwd() )
+
+def run():
+
+    print(pyauto.locateOnScreen('A.png'))
+
+
+
+
+
+if __name__ == '__main__':
+    setup()
+    run()
+
+
+
+#解决相对路径的问题
