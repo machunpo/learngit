6e6833a12c72bf1d2bbb7773faf3fa0d8d2778ef
machunpo <machunpo@aliyun.com>
2020-03-17 20:49:01 +0800

    #下一步 学习一下for和break的用法 对付 一直刷不出来

--- jdtest/jd_test.py
+++ jdtest/jd_test.py
@@ -3,7 +3,7 @@ import time
 import os
 import random
 
-loop_time=80                        #运行的次数
+loop_time=10                        #运行的次数
 pyauto.PAUSE=1                      #每次pyauto的延时
 pic_path='jdtest/pic/'              #图片的目录
 flash_times=5                       #刷新的次数
@@ -11,6 +11,9 @@ flash_times=5                       #刷新的次数
 def setup():
     print('屏幕分辨率：',pyauto.size())                                # 当前屏幕的分辨率
     print('当前目录  ：',os.getcwd() )
+
+def end():
+    pyauto.moveTo(10,800)
 
 def run():
     if (pyauto.locateOnScreen( pic_path + 'sqsy.png' )):               # 申请试用
@@ -20,7 +23,7 @@ def run():
     elif (pyauto.locateOnScreen( pic_path + 'bottom_null.png' )):      # button null
         for i in range(flash_times):
             pyauto.hotkey('f5')
-            time.sleep(3)# 延时3秒，用来等待刷新页面
+            time.sleep(5)# 延时3秒，用来等待刷新页面
             if (pyauto.locateOnScreen( pic_path + 'sqsy.png' )):               # 申请试用
                 pyauto.moveTo(pyauto.locateOnScreen( pic_path + 'sqsy.png' ))
                 pyauto.click()
@@ -51,17 +54,25 @@ def run():
     elif (pyauto.locateOnScreen( pic_path + 'fwycqshzs.png' )):         # 服务异常请稍后再试
         pyauto.hotkey('ctrl','w')
         print('服务异常请稍后再试')
-    else:
-        pass#下一步  处理首页按钮显示不出来
+    else:                                                               # 如果最后 什么显示都没有的话
+        for i in range(flash_times):
+            pyauto.hotkey('f5')
+            time.sleep(5)# 延时3秒，用来等待刷新页面
+            if (pyauto.locateOnScreen( pic_path + 'sqsy.png' )):               # 申请试用
+                pyauto.moveTo(10,10)
+                break
+            else:
+                pass # pyauto.hotkey('ctrl','w')
+            #学习一下for和break的用法
 
     time.sleep(5)
 
 if __name__ == '__main__':
     time.sleep(5)                  # 延时5秒，用来等待页面
     setup()
-    for i in range(20):            # 循环执行20个页面
+    for i in range(loop_time):            # 循环执行 loop-timer个页面
         run()
+    end()
 
 
-
-#下一步  处理首页按钮显示不出来   刷新3-4遍应该有效  也有可能一直刷不出来
+#下一步 学习一下for和break的用法 对付 一直刷不出来
