69b8cd93330ffee222585fce8adf8a7a20547120
machunpo <machunpo@aliyun.com>
2020-06-09 14:14:45 +0800

    vc

--- qtoutiao/jrttkxs.py
+++ qtoutiao/jrttkxs.py
@@ -29,13 +29,10 @@ def bjtx():
         if rult['confidence']>0.9 :
             check(360,1000)#点击按钮看视频（关闭界面）
             time.sleep(3)
-<<<<<<< HEAD
             os.system(r'adb pull /sdcard/funtoutiao.png  C:\Users\machunpo\Desktop')#把图片发到桌面，临时处理
-=======
             #check(x,y)#点击**看视频
             #time.sleep(30)
             #os.system('adb shell input keyevent BACK') #maybe
->>>>>>> 0400d31846ab4d1e267b6f091fe9e0b5e019789b
             print(rult)
         else:
             print('相似度有点低啊！')
