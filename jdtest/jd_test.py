7cca0abfec9f01bd5d5260b921e8d3605a584783
machunpo <machunpo@aliyun.com>
2020-12-07 18:55:16 +0800

    未关注啊

--- jdtest/jd_test.py
+++ jdtest/jd_test.py
@@ -71,7 +71,7 @@ def run():
         pyauto.hotkey('ctrl','w')
         print('京享值不符合要求！')
     
-    elif (pyauto.locateOnScreen( pic_path + 'weiguanzhu.png' )):        # 用户未关注！
+    elif (pyauto.locateOnScreen( pic_path + 'dqyhwgzdp.png' )):        # 用户未关注！
         if guanzhu_count > 3 :
             pyauto.hotkey('ctrl','w')
             guanzhu_count=0
