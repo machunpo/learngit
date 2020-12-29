254f669beb026ac13a71efd03fe90762541ea77a
machunpo <machunpo@aliyun.com>
2020-12-29 17:25:39 +0800

    9

--- prime.py
+++ prime.py
@@ -1,15 +1,24 @@
-
+import time
 
 arry=[2]  #将来更改为从文件中倒入数组
-
-print(arry[0])
-
+#print(arry[0])
+x=0
 m=3
 
 while(1):
     for i in arry:
         if (m%i==0):
             break
+        else:
+            x=1
+    if x==1 :
+        arry.append(m)
+        print(m)
+    x=0
+    m=m+2
+
+    time.sleep(1)
+
         
          
 
