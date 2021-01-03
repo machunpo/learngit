5b96a2b5f528a9907bb4da6e8ca2eb1a0e66ae72
machunpo <machunpo@aliyun.com>
2021-01-03 10:28:17 +0800

    #改进效率

--- prime.py
+++ prime.py
@@ -15,12 +15,13 @@ while(1):
 
     if x==1 :
         arry.append(m)
-        #print(m)
+        print(m)
     x=0
     m=m+2
-    print(arry,m)
+    #print(arry,m)
     #time.sleep(1)
 
+#改进效率
         
          
 
