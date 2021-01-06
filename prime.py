5c6ffe9e1fbb6fc0fc6b3a3ebd0dd72d7c34fc9a
machunpo <machunpo@aliyun.com>
2021-01-06 14:08:58 +0800

    123

--- prime.py
+++ prime.py
@@ -8,19 +8,19 @@ for line in f:
     arry.append(int(line.strip()))
 
 f.close()
-#print(arry)
 
-#arry=[2,3,5,7,11,13,17,19]  #将来更改为从文件中倒入数组
-#x=0
 m=arry[-1]+2
 
 while(1):
     for i in arry:
-        x=0
-        if (m%i==0):
+        if (i>(m**0.5)):
             break
         else:
-            x=1
+            x=0
+            if (m%i==0):
+                break
+            else:
+                x=1
 
 
     if x==1 :
@@ -35,7 +35,7 @@ while(1):
     #print(arry,m)
     
 
-#改进效率
+
         
          
 
