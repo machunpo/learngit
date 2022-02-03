a3b8d1e4e593bbb368d5055bfa3aa49d700ca374
machunpo <machunpo@aliyun.com>
2022-02-03 20:31:12 +0800

    为

--- study_python/文件.py
+++ study_python/文件.py
@@ -11,9 +11,17 @@ text = f.read()
 print(text)
 '''
 
+b=[1,2,4,55,'年']
+
+
 f = open('study_python/mcp.txt','w')
 f.write('你好，这是一个文件操作的复习。\n')
 f.write('这是第二句的测试。\n')
+f.write('这是第3句的测试。\n')
+
+for i in b :
+    f.write(str(i)+'\n')
+
 f.close()
 
 print('文件已经关闭。')
