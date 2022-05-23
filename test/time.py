b14b14b9306df74eb324f526907695dedbd70854
machunpo <machunpo@aliyun.com>
2022-05-23 11:10:14 +0800

    方法

--- test/time.py
+++ test/time.py
@@ -21,6 +21,6 @@ print('程序一共运行了：', alltime, ('秒'))
 print(time.localtime().tm_year, "年")
 print(time.localtime().tm_mon, "月")
 
-#  i5:    41.05399990081787 秒
+#  i5:    34.05399990081787 秒
 #  手机： 40.02087473869324 秒
 
