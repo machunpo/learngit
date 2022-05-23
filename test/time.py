c07dd149cac058942bfe9a49ef942a0941e9066b
machunpo <machunpo@aliyun.com>
2022-05-23 11:09:54 +0800

    更高

--- test/time.py
+++ test/time.py
@@ -1,9 +1,15 @@
 import time
 
 ctime1 = time.time()
+
+
 print('test begin')
 
-for i in range(10000000):               #循环1亿次
+
+ctime1 = time.time()
+
+
+for i in range(100000000):  # 循环1亿次
     a = i**2
 
 ctime2 = time.time()
@@ -12,7 +18,9 @@ alltime = ctime2-ctime1
 
 print('程序一共运行了：', alltime, ('秒'))
 
-print(time.localtime().tm_year,"年")
-print(time.localtime().tm_mon ,"月")
+print(time.localtime().tm_year, "年")
+print(time.localtime().tm_mon, "月")
 
-#  i5:3.8秒
+#  i5:    41.05399990081787 秒
+#  手机： 40.02087473869324 秒
+
