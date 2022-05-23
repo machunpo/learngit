f91ec00ac28551584a6916867677e027d3ab9a9c
machunpo <machunpo@aliyun.com>
2022-05-23 08:18:57 +0800

    3.8秒

--- test/time.py
+++ test/time.py
@@ -3,8 +3,8 @@ import time
 ctime1 = time.time()
 print('test begin')
 
-for i in range(1000000):
-    a = i+1
+for i in range(10000000):               #循环1亿次
+    a = i**2
 
 ctime2 = time.time()
 
@@ -14,3 +14,5 @@ print('程序一共运行了：', alltime, ('秒'))
 
 print(time.localtime().tm_year,"年")
 print(time.localtime().tm_mon ,"月")
+
+#  i5:3.8秒
