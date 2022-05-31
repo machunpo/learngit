13d87749397ba61c1a344a4c84aa3504d4940f28
machunpo <machunpo@aliyun.com>
2022-05-31 19:42:37 +0800

    123

---
+++ 爬虫学习/1.py
@@ -0,0 +1,9 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+html = urlopen("http://pythonscraping.com/pages/page1.html")
+
+bsObj = BeautifulSoup(html.read(),'lxml')
+print(bsObj.h1.get_text())
+
