1d0e894800af8872720ce594a1927eac04133404
machunpo <machunpo@aliyun.com>
2022-06-02 14:37:57 +0800

    about lxml 2

--- 爬虫学习/1.py
+++ 爬虫学习/1.py
@@ -1,10 +1,14 @@
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 
 html = urlopen("http://pythonscraping.com/pages/page1.html")
 
-bsObj = BeautifulSoup(html.read(),'html.parser')
+bsObj = BeautifulSoup(html.read(),'lxml')
+#bsObj = BeautifulSoup(html.read(),'html.parser')
+
+
 
 print(bsObj.h1.get_text())
 
