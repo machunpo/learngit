ad956a311ac2485899908b60a6b92846ddd0ca59
machunpo <machunpo@aliyun.com>
2022-06-07 10:06:06 +0800

    改进

--- test/time.py
+++ test/time.py
@@ -11,9 +11,27 @@ ctime1 = time.time()
 
 
 
-list1=[random.randint(1,1000000) for i in range(1000000)] #生成1百万个随机数进行排序
+list1=[random.randint(1,100000) for i in range(100000)] #生成10万个随机数进行排序
 
-for i in range(100000000):  # 循环1亿次
+
+for i in range(len(list1)):
+
+    # 内循环将数组元素与外循环迭代元素进行比较
+    for j in range(0, len(list1) - i - 1):
+
+      # 比较两个相邻元素
+      if list1[j] > list1[j + 1]:
+
+        # 如果元素不是预期顺序则交换元素
+        temp = list1[j]
+        list1[j] = list1[j+1]
+        list1[j+1] = temp
+
+
+
+
+
+for i in range(10000000):  # 循环1千万次
 
     a = i**2
 
