d757a3ac2fc534edccceeecf11c469f5beddfaf1
machunpo <machunpo@aliyun.com>
2022-06-10 15:26:02 +0800

    BeautifulSoup

--- 爬虫学习/1.py
+++ 爬虫学习/1.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.baidu.com")
+#html = urlopen("http://www.baidu.com")
 
 
 
@@ -10,12 +10,27 @@ html = urlopen("https://tianqi.2345.com/")
 bsObj = BeautifulSoup(html.read(),'lxml')
 #bsObj = BeautifulSoup(html.read(),'html.parser')
 
+banner_date=bsObj.findAll('div',{"class":"banner-right-con-list-time"})
+date=bsObj.findAll('div',{"class":"date"})
+weather=bsObj.findAll('div',{"class":"banner-right-con-list-status"})
+temp=bsObj.findAll('div',{"class":"banner-right-con-list-temp"})
+
+print('banner_date:',banner_date[0].get_text())
+print('date:',date[0].get_text())
+print('weather:',weather[0].get_text())
+print('temp:',temp[0].get_text())
 
 
-print(bsObj.head.title)
+for i in range(len(weather)):
 
-
+    print(banner_date[i].get_text(),date[i].get_text(), weather[i].get_text(),temp[i].get_text() )
 print(bsObj.head.title.get_text())
+
+
+
+#  <div class="banner-right-con-list-time">
+#                  明天
+#                </div>
 
 
 
