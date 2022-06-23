d938f60e994e1cf939b1e1de19a136b29fba7476
machunpo <machunpo@aliyun.com>
2022-06-23 10:40:39 +0800

    weather

--- 爬虫学习/1.py
+++ 爬虫学习/1.py
@@ -1,25 +1,26 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-#html = urlopen("http://www.baidu.com")
 
 
-
-html = urlopen("https://tianqi.2345.com/")
+html = urlopen("https://tianqi.2345.com/today-70445.htm")
 
 bsObj = BeautifulSoup(html.read(),'lxml')
 #bsObj = BeautifulSoup(html.read(),'html.parser')
 
-banner_date=bsObj.findAll('div',{"class":"banner-right-con-list-time"})
-date=bsObj.findAll('div',{"class":"date"})
-weather=bsObj.findAll('div',{"class":"banner-right-con-list-status"})
-temp=bsObj.findAll('div',{"class":"banner-right-con-list-temp"})
+#banner_date=bsObj.findAll('div',{"class":"banner-right-con-list-time"})
+date=bsObj.findAll('a',{"class":"seven-day-item yes"})
+#weather=bsObj.findAll('font')
+#temp=bsObj.findAll('div',{"class":"banner-right-con-list-temp"})
 
-print('banner_date:',banner_date[0].get_text())
-print('date:',date[0].get_text())
-print('weather:',weather[0].get_text())
-print('temp:',temp[0].get_text())
+#print('banner_date:',banner_date[0].get_text())
+print(html)
+print(bsObj)
+#print(weather)
 
+
+#print('temp:',temp[0].get_text())
+'''
 
 for i in range(len(weather)):
 
@@ -31,6 +32,6 @@ print(bsObj.head.title.get_text())
 #  <div class="banner-right-con-list-time">
 #                  明天
 #                </div>
-
+'''
 
 
