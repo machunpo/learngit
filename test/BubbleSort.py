5fd4a96888cf9ddb1ec1ede440e284e64f71de44
“machunpo” <machunpo@aliyun.com>
2024-02-23 11:03:59 +0800

    6

--- test/BubbleSort.py
+++ test/BubbleSort.py
@@ -1,25 +1,21 @@
 import random
 
 list1=[random.randint(1,100) for i in range(20)] #列表解析
-#生成1万个随机数进行排序
+#生成20个100以内随机数进行排序
 
 print(list1)
 print('')
 
 for i in range(len(list1)):
-   #print(list1)
 
-    # 内循环将数组元素与外循环迭代元素进行比较
+  # 内循环将数组元素与外循环迭代元素进行比较
   for j in range(0, len(list1) - i - 1):
 
-      # 比较两个相邻元素
+    # 比较两个相邻元素
     if list1[j] > list1[j + 1]:
 
-        # 如果元素不是预期顺序则交换元素
-        #temp = list1[j]
-        #list1[j] = list1[j+1]
-        #list1[j+1] = temp
+      # 如果元素不是预期顺序则交换元素
       list1[j],list1[j+1] = list1[j+1] ,list1[j] 
   
   print(list1)
-print(list1)
+#print(list1)
